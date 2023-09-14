fbb3d6d4cb78d72f88a77abeb2329f49edab8ebc
Ryandi Aziz <ryandiaziz.edu@gmail.com>
2023-09-14 08:10:55 +0700

    added some improvement

--- server/routes/articleRoute.js
+++ server/routes/articleRoute.js
@@ -1,15 +1,7 @@
 const articleRoute = require('express').Router()
 const { ArticleController } = require("../controller")
-// const { upload } = require('../middleware/configUpload')
-// const { auth } = require('../middleware/auth')
-
 
 articleRoute.get('/', ArticleController.getArticles)
 articleRoute.get('/search', ArticleController.searchArticles)
-// articleRoute.get('/likes', auth, ArticleController.getAccount)
-// articleRoute.post('/create', ArticleController.createUser)
-// articleRoute.post('/login', ArticleController.login)
-// articleRoute.put('/update/:id', auth, upload, ArticleController.update)
-// articleRoute.delete('/delete/:id', ArticleController.delete)
 
 module.exports = articleRoute
--- server/routes/userArticleRoute.js
+++ server/routes/userArticleRoute.js
@@ -5,8 +5,5 @@ const { auth } = require('../middleware/auth')
 userArticleRoute.post('/', auth, UserArticleController.addLikedArticle)
 userArticleRoute.get('/', auth, UserArticleController.getLikedArticles)
 userArticleRoute.delete('/:articleId', auth, UserArticleController.removeLikedArticle)
-// userRoute.post('/', UserArticleController.createUser)
-// userRoute.post('/login', UserArticleController.login)
-// userRoute.get('/account', auth, UserArticleController.getAccount)
 
 module.exports = userArticleRoute
