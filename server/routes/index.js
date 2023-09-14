b3091805a8abf2cd4e3c4c1482d9e8a6f45de0cd
Ryandi Aziz <ryandiaziz.edu@gmail.com>
2023-09-14 10:56:02 +0700

    updated

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,18 @@ const userRoutes = require('./userRoute')
 const articleRoute = require('./articleRoute')
 const userArticleRoute = require('./userArticleRoute')
 
+route.get('/', (req, res) => {
+    res.json({
+        status: "ok",
+        message: "welcome to news-app API"
+    })
+})
+route.get('/api', (req, res) => {
+    res.json({
+        status: "ok",
+        message: "welcome to news-app API"
+    })
+})
 route.use('/api/users', userRoutes)
 route.use('/api/articles', articleRoute)
 route.use('/api/userarticle', userArticleRoute)
