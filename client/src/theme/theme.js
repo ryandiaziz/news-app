640f3a699055fd8de36e11d5dfc68bc5c7f9ce36
Ryandi Aziz <ryandiaziz.edu@gmail.com>
2023-09-15 00:40:20 +0700

    add appbar

---
+++ client/src/theme/theme.js
@@ -0,0 +1,11 @@
+import { createTheme } from '@mui/material'
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#353535',
+        }
+    }
+})
+
+export default theme
